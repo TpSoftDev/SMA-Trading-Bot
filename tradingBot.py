71541d4fb16aad04c5068fada14b873acc209993
TpSoftDev <tpsoftwaredev@gmail.com>
2024-09-15 20:51:34 -0500

    intialize MetaTrader Account using MT5 API

---
+++ tradingBot.py
@@ -0,0 +1,24 @@
+import MetaTrader5 as mt5
+
+# Import utility functions for trading operations
+from mt5_trade_utils import send_market_order, close_position, close_all_positions
+from time import sleep
+
+if __name__ == "__main__":
+    # Initialize connection to MetaTrader 5
+    mt5.initialize()
+
+    # Define login credentials and server information
+    login = 25141208
+    password = 'l4!z9,JM5>bl'  # Replace with your actual password
+    server = 'Tickmill-Demo'    # Replace with your actual server
+
+    # Log in to MetaTrader 5 with provided credentials
+    mt5.login(login, password, server)
+
+    # Wait 5 seconds after logging in
+    sleep(5)
+    send_market_order()
+
+
+
